e7710a38329021abd97cfc786fb5d16ad8095097
Mark Venema <markavenema@gmail.com>
2018-04-09 13:57:47 +0900

    Removed devise secret key

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module DevcampPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     
 
