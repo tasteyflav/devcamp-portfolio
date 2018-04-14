f5dc9b27e2ab259591b2019eb24b14e66c168a38
Mark Venema <markavenema@gmail.com>
2018-04-14 17:19:24 +0900

    Modified Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :name
+  # validates_presence_of :name
   def first_name
   	self.name.split.first
   end
