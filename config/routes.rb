393535517f7a60f965fc24d8268e78eea8ab1e2b
Mark Venema <markavenema@gmail.com>
2018-04-23 07:23:20 +0900

    Implemented twitter call on tech news page

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
-
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
   	member do
