6eaa70ac1000e73f90967557c4e7939e84811fab
Mark Venema <markavenema@gmail.com>
2018-04-24 18:26:39 +0900

    Configured action cable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https:www.markeigo.com', 'http://www.markeigo.com']
+  config.action_cable.url = "wss://www.markeigo.com/cable"
 end
